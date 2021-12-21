eaba9b25a01432d9d935a31bb8d71e99b557b982
Vlastimil Vašut <Vasut.Vlastimil@stud.vassboskovice.cz>
2021-12-21 11:35:30 +0100

    Přidejte soubory projektu.

---
+++ Vánoční stromek/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace Vánoční_stromek
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+          
+            Random random = new Random();
+            int cislo = random.Next(0, 4);
+            cislo++;
+            if (cislo == 0)
+            {
+                B02.Source = (ImageSource)FindResource("white");
+            }
+        else if (cislo == 1)
+            {
+                B02.Source = (ImageSource)FindResource("red");
+            }
+        }
+    }
+}
